2a35ee7e63e11113acda8ed4464bf2d86cc4152b
Tobi Tijani <tobi_tijani@yahoo.com>
2023-09-28 18:37:46 -0400

    Pascal Triangle task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle.py
+This module contains the function pascal_triangle, returns a list of lists of integers
+representing the Pascal's triangle of n
+"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    pascal_tri = [[1]]
+
+    for i in range(1, n):
+        prev_row = pascal_tri[-1]
+        new_row = [1]
+
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)
+        pascal_tri.append(new_row)
+
+    return pascal_tri
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,2 @@
+# 0x00. Pascal's Triangle
+Pascal's Triangle Task
