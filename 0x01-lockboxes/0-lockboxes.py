dc59f8c41412a4cc34dc6a1c1cd4b9a1a3dfdf8b
Tobi Tijani <tobi_tijani@yahoo.com>
2023-10-05 13:58:10 -0400

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+0-lockboxes.py
+This module contains the function `def canUnlockAll(boxes)`,
+which determines if all the boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    You have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1 and
+    each box may contain box_keys to the other boxes. Write a
+    method that determines if all the boxes can be opened
+    """
+    def canUnlockAllRec(boxes, unlocked, box):
+        """Recursive function for canUnlockAll"""
+        if len(box) == 0:
+            return
+
+        for i in range(len(box)):
+            if box[i] in unlocked or box[i] >= len(boxes):
+                continue
+            if box[i] != 0:
+                unlocked.append(box[i])
+            canUnlockAllRec(boxes, unlocked, boxes[box[i]])
+
+    unlocked = []
+    box_keys = []
+
+    if len(boxes[0]) == 0:
+        if len(boxes) == 1:
+            return True
+        return False
+
+    for i in range(1, len(boxes)):
+        box_keys.append(i)
+
+    canUnlockAllRec(boxes, unlocked, boxes[0])
+
+    if set(unlocked) == set(box_keys):
+        return True
+    return False
