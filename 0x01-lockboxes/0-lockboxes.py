8be53ad4c09e63c87175a81773b257bdfc3004e0
Tobi Tijani <tobi_tijani@yahoo.com>
2023-10-05 17:03:52 -0400

    Improvements to code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,35 +13,18 @@ def canUnlockAll(boxes):
     each box may contain box_keys to the other boxes. Write a
     method that determines if all the boxes can be opened
     """
-    def canUnlockAllRec(boxes, unlocked, box):
-        """Recursive function for canUnlockAll"""
-        if len(box) == 0:
-            return
-
-        for i in range(len(box)):
-            if box[i] in unlocked or box[i] >= len(boxes):
-                continue
-            if box[i] != 0:
-                unlocked.append(box[i])
-            if box[i] == i and len(box) == 1:
-                return
-            canUnlockAllRec(boxes, unlocked, boxes[box[i]])
-
-    unlocked = []
-    number_boxes_to_unlock = len(boxes) - 1
-
-    # print('All boxes:', boxes)
-
-    if len(boxes) == 0:
-        return False
-    if len(boxes[0]) == 0:
-        if len(boxes) == 1:
-            return True
+    if not boxes:
         return False
 
-    canUnlockAllRec(boxes, unlocked, boxes[0])
+    n = len(boxes)
+    unlocked = set([0])
+    queue = [0]
 
-    # print('Boxes unlocked:', unlocked)
-    if len(unlocked) == number_boxes_to_unlock:
-        return True
-    return False
+    while queue:
+        current_box = queue.pop()
+        for key in boxes[current_box]:
+            if key < n and key not in unlocked:
+                unlocked.add(key)
+                queue.append(key)
+
+    return len(unlocked) == n
